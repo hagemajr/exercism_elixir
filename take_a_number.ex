25c42f1da4ef58baa4bd630d41ad5cd9a8b37d53
hagemajr <john.r.hageman@gmail.com>
2024-05-16 16:30:55 -0500

    Create take_a_number.ex

---
+++ take_a_number.ex
@@ -0,0 +1,18 @@
+defmodule TakeANumber do
+  def start() do   
+    spawn(fn -> loop(0) end)
+  end
+
+  defp loop(ticket_num) do
+    receive do
+      {:report_state, sender_pid} -> 
+        send(sender_pid, ticket_num)
+        loop(ticket_num)
+      {:take_a_number, sender_pid} -> 
+        send(sender_pid, ticket_num + 1)
+        loop(ticket_num + 1)
+      :stop -> ticket_num
+      _ -> loop(ticket_num)
+    end
+  end
+end
